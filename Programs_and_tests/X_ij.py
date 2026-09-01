a937d6cdfd9b719095ae92a24953ebd6d83dc690
Lore-Saba <l.sabadini@campus.unimib.it>
2026-09-02 00:15:47 +0200

    Clean up imports and refactor QuantumCircuit class
    Refactor code by removing unused imports and adding necessary imports for functionality. Adjust class and function definitions for better organization and readability.

--- Programs_and_tests/X_ij.py
+++ Programs_and_tests/X_ij.py
@@ -17,9 +17,7 @@ import sympy #serve per creare simboli che rappresentano parametri variabili nei
 
 
 from typing import Union
-"""import warnings
-# Forza Python a lanciare un'eccezione (crash) appena incontra un UserWarning
-warnings.filterwarnings('error', category=UserWarning)"""
+
 
 #importo i gates per i qudit di dimensione d
 #(la dimensione d è impostata di default uguale a 3)
@@ -47,8 +45,6 @@ class QuantumCircuit:
         self.circuit = cirq.Circuit()
 
         # Definisco i parametri variazionali usando i simboli di sympy.
-        # Definisco d parametri dato che apllico rotazioni alle coppie di livelli
-        # energetici (per d=3 --> R_01, R_12, R_20)
         self.params = [sympy.Symbol(f'p{i}') for i in range(dimension)]
 
         # Definisco il curcuito
@@ -56,7 +52,7 @@ class QuantumCircuit:
         for i in range(self.dimension-1):
             self.circuit.append(X_P_ij(i,i+1, dimension= self.dimension ,phase=self.params[i]).on(self.qudit))
         self.circuit.append(X_P_ij(self.dimension-1,0 , dimension= self.dimension, phase=self.params[self.dimension-1]).on(self.qudit))
-        #self.circuit.append(H_d(dimension=self.dimension).on(self.qudit))
+       
         # Misuro il qudit 0 con chiave 'm'
         self.circuit.append(cirq.measure(self.qudit, key='m'))
         print(self.circuit)
@@ -83,7 +79,6 @@ class QuantumCircuit:
             param_resolver = {self.params[k]: thetas_list[k] for k in range(self.dimension)}
 
             #simulazione del circuito per un numero di volte pari a self.shots, usando i parametri risolti
-            #ovvero i parametri ai quali viene associato un valore numerico
             result = cirq.Simulator().run(self.circuit, param_resolver=param_resolver, repetitions=self.shots)
 
             #conto dei risultati
@@ -100,18 +95,16 @@ print(C.run([np.pi/2]*global_dimension))
 print(C.view_val_prob())"""
 
 class HybridFunction(Function):
-     @staticmethod #Nota: trovi spiegazioni su questo comando nelle note salvate su Notebook llm
+     @staticmethod
      def forward(ctx, input, quantum_circuit, shift, dimension):
         # ctx è un oggetto che viene creato ed inserito automaticamente da PyTorch 
-        # quando eseguiamo il forward pass. Serve come canale di comunicazione per conservare
-        # le informazioni del forward che saranno indispensabili per calcolare il gradiente nel backward.
-        # Salvo nel contesto 'ctx' i parametri necessari per il calcolo dei gradienti (backward)
+        # quando eseguiamo il forward pass.
         ctx.shift = shift
         ctx.quantum_circuit = quantum_circuit
         ctx.dimension = dimension   
          
         # salvo l'input come tensore PyTorch per poterlo usare nel backward pass
-        ctx.save_for_backward(input) #è un metodo integrato dell'oggetto ctx, fornito nativamente da Autograd
+        ctx.save_for_backward(input) 
          
         # eseguo il circuito quantistico di Cirq passando gli angoli e calcolo la probabilità
         vector_prob = ctx.quantum_circuit.run(input)
@@ -119,7 +112,7 @@ class HybridFunction(Function):
         # converto il risultato ottenuto in un tensore
         result = torch.tensor([vector_prob], dtype=torch.float32)
         # Il fatto che usiamo una parentesi quadra oltre quella di vector_prob
-        # è perchè la crossentropyloss richiede una fromattazione specifica (vedi note Notebook llm)
+        # è perchè la crossentropyloss richiede una fromattazione specifica 
         return result
          
      @staticmethod
@@ -144,28 +137,14 @@ class HybridFunction(Function):
             
             for i in range(dimension): 
                 gradients.append((expectation_right[i] - expectation_left[i])/2.0)
-            # Per ogni parametro ottengo un vettore d-dim contenente le derivate di
-            # di tutte le probabilità rispetto al parametro.
-            # In pratica ho una matrice corrispondente allo Jacobiano
 
-        # La funzione calc_prob restituisce degli array NumPy quindi gradients
-        # sarà un array di NumPy array ognguno in un punto diverso della memoria. 
-        # Questo rallenta il processo, per ottimizzarlo scriviamo np.array(gradients) 
-        # così da avere un unico array NumPy. Così facendo, trasformarlo in tensore con 
-        # torch.tensor() sarà molto più efficiente. 
-        #(NON è DETTO CHE SIA ANCORA COSì)
         gradients_tensor = torch.tensor(np.array(gradients), dtype=torch.float32)
 
         #Per la Chain Rule moltiplico il gradiente in uscita dai layer successivi 
         # per la matrice jacobiana del layer quantistico
-        #print('grad output:',grad_outputs.squeeze())
-        #print('gradients', gradients_tensor.reshape(len(input_list), dimension))
         Jacobian_matrix =  gradients_tensor.reshape(len(input_list), dimension)
         grad_input_matrix = Jacobian_matrix* grad_outputs.squeeze()
-        grad_input = torch.sum(grad_input_matrix, -1) # Il -1 fa si che la somma sia solo
-        # per gli elementi delle righe.
-        # Devo fare questa somma perchè per Mat*Vect Pytorch mi restituisce una matrice,
-        # non somma in auomatico gli elementi delle righe come per un prodotto matrice-vettore
+        grad_input = torch.sum(grad_input_matrix, -1) # Il -1 fa si che la somma sia solo per gli elementi delle righe.
         
         # Ritorno il gradiente con lo stesso shape dell'input iniziale,
         # e 'None' per gli altri argomenti non addestrabili di forward() (quantum_circuit, shift e dimension)
@@ -182,9 +161,7 @@ class Hybrid(nn.Module):
 
      def forward(self, input):
           return HybridFunction.apply(input, self.quantum_circuit, self.shift, self.dimension)
-          # .apply() crea l'oggetto ctx, nel quale salviamo i parametri 
-          # e le vaire istanze (.shift, .quantum_circuit) che servono 
-          # per il calcolo dei gradienti, ed esegue il forward
+          # .apply() crea l'oggetto ctx
 
 def show_img(X):
     image, label = X
@@ -201,23 +178,17 @@ n_samples = 100
 x_train = datasets.MNIST(root='./data', train=True, download=True,
                           transform=transforms.Compose([transforms.ToTensor()]))
 
-# filtro solo le cifre 0 e 1 con un numero di elementi pari a n_samples per ogni classe
-# il comando np.where ci da liste di indici  
+# filtro solo le cifre 0, 1 e 2 con un numero di elementi pari a n_samples per ogni classe
 idx = np.concatenate([
     np.where(x_train.targets == 0)[0][:n_samples],
     np.where(x_train.targets == 1)[0][:n_samples],
     np.where(x_train.targets == 2)[0][:n_samples]
     ])
-# i dataset supportano la funzione di indexing avanzato, passando un numpy array o un tensore
-# verrà interpretato come lista di indici. Così facendo i training data saranno solo quelli
-# selezionati
+
 x_train.data= x_train.data[idx]
 x_train.targets = x_train.targets[idx]
-#show_img(x_train[107])
-#print(x_train.targets[101])
 
 #creo il DataLoader per pytorch
-# Nota: trovi spiegazioni su questo comando nelle note salvate su Notebook llm
 train_loader = torch.utils.data.DataLoader(x_train, batch_size=1, shuffle= True)
 
 #costruisco la rete neurale
@@ -226,20 +197,16 @@ class Net(nn.Module):
           super().__init__()
           self.layer_1 = nn.Linear(784,128) #passa 784 pixel a 128 neuroni
           self.layer_2 = nn.Linear(128,64) #riduco ulteriormente le dimensioni a 64 neuroni
-          self.layer_3 = nn.Linear(64, dimension) #l'ultima riduzione fa si che i parametri
-          #di output del secondo layer diventino dello stesso numero della dimensione del qudit
+          self.layer_3 = nn.Linear(64, dimension) #l'ultima riduzione fa si che i parametri di output del secondo layer diventino dello stesso numero della dimensione del qudit
           self.hybrid = Hybrid(dimension, shots, shift)
      def forward(self, x):
           # Appiattisco l'immagine da (1, 28, 28) a (1, 784)
-          # Il 784 dice a PyTorch in quante colonne ridistribuire i dati,
-          # il -1 gli dice di calcolare da solo quante righe servono per farlo. 
           x = x.view(-1, 784)
           x = F.relu(self.layer_1(x))
           x = F.relu(self.layer_2(x))
-          # Non applichiamo la ReLU qui perché gli angoli di rotazione quantistici 
-          # devono poter assumere anche valori negativi (da -pi a pi).
+          # Non applico la ReLU qui perché gli angoli di rotazione quantistici devono poter assumere anche valori negativi (da -pi a pi).
           x = self.layer_3(x) 
-          # Passaggio nel simulatore quantistico Cirq per estrarre la probabilità P(1)
+          # Passaggio nel simulatore quantistico
           x = self.hybrid(x)
           # restituisco il vettore di probabilità (P(0), P(1) ...)
           return x 
@@ -265,7 +232,7 @@ def training_loop(n_epochs, optim, model, loss_func, train_loader):
 
                total_loss.append(loss.item())
 
-          end_time = time.perf_counter()    # 2. Registra il tempo di fine epoca
+          end_time = time.perf_counter()    # Registra il tempo di fine epoca
           elapsed_time = end_time - start_time # Tempo totale dell'epoca in secondi
           epoch_times.append(elapsed_time)
 
@@ -279,7 +246,7 @@ def training_loop(n_epochs, optim, model, loss_func, train_loader):
 
      return loss_values
 
-# Set manual seed since nn.Parameter are randomly initialized
+# Fisso il seed di pytorch per introdurre riproducibilità
 torch.manual_seed(42) 
 
 avg_time_perf = []
@@ -288,6 +255,7 @@ acc_perf = []
 
 number_of_test=10
 
+# costruisco un ciclo per testare più volte l'addestramento della rete neurale
 for i in range(number_of_test):
     model = Net(global_dimension, global_shots, shift)
     params = list(model.parameters())
@@ -329,7 +297,7 @@ for i in range(number_of_test):
     test_loader = torch.utils.data.DataLoader(x_test, batch_size=1, shuffle=False)
 
     def validate(model, test_loader, loss_func):
-        model.eval()# 1. Disattiva Dropout e imposta la rete in modalità test
+        model.eval()# Disattiva Dropout e imposta la rete in modalità test
         test_loss = 0
         correct = 0
         with torch.no_grad(): #disattivo il calcolo dei gradienti
